6d79b425361655cb64adae458924f99630788bd4
Eva Niehaus <eniehaus111@gmail.com>
2023-07-17 00:27:23 +0200

    add hw 2 fixed onClick

--- src/HW/2/HW2.tsx
+++ src/HW/2/HW2.tsx
@@ -47,7 +47,7 @@ export const HW2 = () => {
     let [currentUsers, setCurrentUsers] = useState<UsersObjectType>(users);
 
     const filterUsers = () => {
-        const filteredUsers = users.myFriends.filter(user => user.address.city === 'Los Angeles') //'НУЖНО ПРОФИЛЬТРОВАТЬ ДРУЗЕЙ. ОСТАВЛЯЕМ ТОЛЬКО ТЕХ, КОТОРЫЕ ЖИВУТ В ГОРОДЕ LOS ANGELES';
+        const filteredUsers = currentUsers.myFriends.filter(user => user.address.city === 'Los Angeles') //'НУЖНО ПРОФИЛЬТРОВАТЬ ДРУЗЕЙ. ОСТАВЛЯЕМ ТОЛЬКО ТЕХ, КОТОРЫЕ ЖИВУТ В ГОРОДЕ LOS ANGELES';
         setCurrentUsers({myFriends: filteredUsers});
 
     };
